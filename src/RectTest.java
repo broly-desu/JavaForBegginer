0eed9086a6a16989893a026a7bb712fc30958118
achills deed <js7201028@icloud.com>
2025-05-26 10:15:56 +0900

    答え合わせ分

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,19 +1,25 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		setColor(255,0,0);
-		fillRect(30,40,50,100);
+		setColor(0,255,255);
+		fillRect(30,40,50,50);
 		
-		setColor(0,255,0);
-		fillRect(130,40,50,100);
+		setColor(255,0,255);
+		fillRect(90,40,50,50);
 		
-		setColor(0,0,255);
-		fillRect(30,200,150,10);
+		setColor(0,255,255);
+		fillRect(150,40,50,50);
 		
-		setColor(255,255,0);
-		fillRect(90,160,30,30);
+		setColor(0,0,0);
+		fillRect(30,100,50,50);
+		
+		setColor(128,128,128);
+		fillRect(90,100,50,50);
+		
+		setColor(0,0,0);
+		fillRect(140,90,70,70);
 		
 		setColor(255,255,255);
-		fillRect(30,250,150,150);
+		fillRect(150,100,50,50);
 		}
 }
